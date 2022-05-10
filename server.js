b9a9dad3f6579b8e455c4175ac50d253ffbcb246
foxcode <sebeklis132@gmail.com>
2022-05-10 14:45:07 +0200

    dziala

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const path = require('path');
+const  { WebSocketServer } = require('ws')
 
-const port = 6969;
-const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocketServer({
+    port: 6970
+  });
+const app = express();
+const port = process.env.PORT || 6969;
+app.use(express.static(__dirname + '/public'));
+wss.on("connection", ws => {
+  ws.on("message", data => {
 
-wss.on('connection', function connection(ws) {
-
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+    wss.clients.forEach((client)=>{
+      if(client != ws){
+        client.send(`${data}`)
       }
     })
-  })
-})
 
-server.listen(port, function() {
-  console.log(`Server is listening on ${port}!`)
-})
+    console.log(`Client has sent us: ${data}`)
+  });
 
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
