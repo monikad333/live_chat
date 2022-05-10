2e051254debcba9b07088278ed0a7dba44d1d4bd
foxcode <sebeklis132@gmail.com>
2022-05-10 15:13:33 +0200

    hotfix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const  { WebSocketServer } = require('ws')
 
 const wss = new WebSocketServer({
-    port: 20103
+    port: 8081
   });
 const app = express();
 const port = process.env.PORT || 8080;
